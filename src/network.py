c611407910e76507037ae544a4d21d279defd597
Zack B) <47122199+zbfromztl@users.noreply.github.com>
2024-06-08 02:34:04 -0400

    Update network.py

--- src/network.py
+++ src/network.py
@@ -3,5 +3,13 @@ import socket
 #Well, that's all folks!
 class Network():
   def __init__(self) -> None:
-    print("Initializing network...")
-    #
+    #Config network data.
+    port = "9511"
+    print("Initializing electronic flight strip transfer system configurator...")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #Determine "where" server is.
+    server_ip = ""
+    server_ip = input("Please input the IP of the server. Leave blank if this machine is the server: ")
+    if server_ip = "":
+      server_ip = socket.gethostname()
+    print(f"Server IP set to {server_ip}.")
