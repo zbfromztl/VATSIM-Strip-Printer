f96877851a04b6b636a99c18314f04b74848e36b
Zack B) <47122199+zbfromztl@users.noreply.github.com>
2024-06-08 02:45:17 -0400

    Update network.py

--- src/network.py
+++ src/network.py
@@ -4,6 +4,7 @@ import socket
 class Network():
   def __init__(self) -> None:
     #Config network data.
+    Privacy_mode = False
     port = "9511"
     print("Initializing electronic flight strip transfer system configurator...")
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,9 +13,21 @@ class Network():
     server_ip = input("Please input the IP of the server. Leave blank if this machine is the server: ")
     if server_ip = "":
       server_ip = socket.gethostname()
-    print(f"Server IP set to {server_ip}.")
+    if Privacy_mode = false:
+      print(f"Server IP set to {server_ip}.")
     departure_printers = set()
     
     #TODO: Add logic to connect to server if it isn't "this" machine.
     #TODO: Add logic to process printer connecting/unresponsive(disconnect) to server.
     #TODO: Process "send strip to departure" message. 
+
+  def run_server(self):
+    s = self.s
+    try:  #This logic is flawed (it throws an error and then continues to attempt the server lol)
+      s.bind(int(self.server_ip), int(self.port))
+      s.listen(6)
+    except:
+      print(Unable to initialize server.)
+    while True:
+      clientsocket, address = s.accept()
+      
