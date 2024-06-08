2fe51a1de6d325f41113606d6a2819c2eddadf3b
Zack B) <47122199+zbfromztl@users.noreply.github.com>
2024-06-08 07:26:12 -0400

    Add files via upload

--- src/network.py
+++ src/network.py
@@ -5,29 +5,29 @@ class Network():
   def __init__(self) -> None:
     #Config network data.
     Privacy_mode = False
-    port = "9511"
+    server_port = "9511"
     print("Initializing electronic flight strip transfer system configurator...")
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     #Determine "where" server is.
     server_ip = ""
     server_ip = input("Please input the IP of the server. Leave blank if this machine is the server: ")
-    if server_ip = "":
+    if server_ip == "":
       server_ip = socket.gethostname()
-    if Privacy_mode = false:
+    if Privacy_mode == False:
       print(f"Server IP set to {server_ip}.")
-    departure_printers = dict()
+    network_devices = dict()
     
     #TODO: Add logic to connect to server if it isn't "this" machine.
     #TODO: Add logic to process printer connecting/unresponsive(disconnect) to server.
     #TODO: Process "send strip to departure" message. 
 
   def run_server(self):
-    server_socket = self.s
+    server_socket = self.server_socket
     try:  #This logic is flawed (it throws an error and then continues to attempt the server lol)
-      server_socket.bind(int(self.server_ip), int(self.port))
+      server_socket.bind(int(self.server_ip), int(self.server_port))
       server_socket.listen(6)
     except:
-      print(Unable to initialize server.)
+      print("Unable to initialize server.")
     while True:
-      clientsocket, address = s.accept()
+      clientsocket, address = server_socket.accept()
       
